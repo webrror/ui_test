017dc6bb791b25b5e459f321eef7f43bd99e12ed
Jerin <jerinjacob0206@gmail.com>
2023-01-17 14:22:40 +0530

    fixed scrolling

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -7,98 +7,131 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(FluentIcons.line_horizontal_3_20_regular),
-          onPressed: () {},
-        ),
-        title: const ListTile(
-          contentPadding: EdgeInsets.zero,
-          title: Text(
-            "PRINCIPAL'S DESK",
-            style: TextStyle(color: Colors.grey, fontSize: 14),
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          floating: true,
+          pinned: true,
+          leading: IconButton(
+            icon: const Icon(FluentIcons.line_horizontal_3_20_regular),
+            onPressed: () {},
           ),
-          subtitle: Text("Eloit Academy, New Delhi",
-              style: TextStyle(color: Colors.black, fontSize: 17)),
-        ),
-        actions: const [
-          CircleAvatar(
-            backgroundImage: NetworkImage(
-                'https://images.unsplash.com/photo-1673833257045-bd91f5e84fae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'),
+          title: const ListTile(
+            contentPadding: EdgeInsets.zero,
+            title: Text(
+              "PRINCIPAL'S DESK",
+              style: TextStyle(color: Colors.grey, fontSize: 14),
+            ),
+            subtitle: Text("Eloit Academy, New Delhi",
+                style: TextStyle(color: Colors.black, fontSize: 17)),
           ),
-          SizedBox(
-            width: 20,
-          )
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text("My Updates"),
-            const SizedBox(
-              height: 20,
+          actions: const [
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://images.unsplash.com/photo-1673833257045-bd91f5e84fae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'),
             ),
-            Expanded(
-                child: GridView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3),
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.blue,
-                        ),
-                        child: SizedBox(
-                          height: 60,
-                          width: 60,
-                          child: myUpdates[index]['icon'],
-                        )),
-                    Text(myUpdates[index]['title'])
-                  ],
-                );
-              },
-              itemCount: myUpdates.length,
-            )),
-            const Text("My Updates"),
-            const SizedBox(
-              height: 20,
-            ),
-            Expanded(
-                child: GridView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3),
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.blue,
-                        ),
-                        child: SizedBox(
-                          height: 60,
-                          width: 60,
-                          child: myUpdates[index]['icon'],
-                        )),
-                    Text(myUpdates[index]['title'])
-                  ],
-                );
-              },
-              itemCount: myUpdates.length,
-            )),
+            SizedBox(
+              width: 20,
+            )
           ],
         ),
-      ),
+        const SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20),
+            child: Text("My Udpates"),
+          ),
+        ),
+        SliverGrid(
+          delegate: SliverChildBuilderDelegate((context, index) {
+            return Column(
+              children: [
+                Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.blue,
+                    ),
+                    child: SizedBox(
+                      height: 60,
+                      width: 60,
+                      child: myUpdates[index]['icon'],
+                    )),
+                Text(myUpdates[index]['title'])
+              ],
+            );
+          }, childCount: myUpdates.length),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+        ),
+        const SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 20),
+            child: Text("My Udpates"),
+          ),
+        ),
+        SliverGrid(
+          delegate: SliverChildBuilderDelegate((context, index) {
+            return Column(
+                  children: [
+                    Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.blue,
+                        ),
+                        child: SizedBox(
+                          height: 60,
+                          width: 60,
+                          child: myUpdates[index]['icon'],
+                        )),
+                    Text(myUpdates[index]['title'])
+                  ],
+                );
+                
+          },
+            childCount: myUpdates.length),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+        ),
+        const SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 10),
+            child: Text("My Udpates"),
+          ),
+        ),
+        SliverGrid(
+          delegate: SliverChildBuilderDelegate((context, index) {
+            return Column(
+                  children: [
+                    Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.blue,
+                        ),
+                        child: SizedBox(
+                          height: 60,
+                          width: 60,
+                          child: myUpdates[index]['icon'],
+                        )),
+                    Text(myUpdates[index]['title'])
+                  ],
+                );
+                
+          },
+            childCount: myUpdates.length),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+        )
+        // SliverList(
+        //     delegate: SliverChildListDelegate([
+        //   GridView.builder(
+        //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        //         crossAxisCount: 3),
+        //     itemBuilder: (context, index) {
+        //       return Column();
+        //     },
+        //     itemCount: myUpdates.length,
+        //   )
+        // ]))
+      ],
     );
   }
 }
